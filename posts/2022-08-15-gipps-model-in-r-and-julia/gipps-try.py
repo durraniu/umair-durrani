ca0d04b270de76e5b0141dc09b8724491e953212
Umair Durrani <durraniu@uwindsor.ca>
2022-08-29 11:55:17 -0400

    few changes

---
+++ posts/2022-08-15-gipps-model-in-r-and-julia/gipps-try.py
@@ -0,0 +1,136 @@
+# Load libraries---------
+import numpy as np
+import pandas as pd
+
+
+# Load data---------------
+dfn1 = pd.read_csv("dfn1.csv")
+
+
+resolution = 0.1
+N = 1
+xn1 = 'PrecVehLocalY'
+vn1 = 'PrecVehVel'
+ln = list(np.unique(dfn1['PrecVehLength']))  #list(dfn1['Local.Y'][0])
+
+
+
+
+####### Time #############################################
+
+# Last time frame of the simulation:
+last_time = (dfn1.shape[0] - 1) * resolution
+
+# Time vector:
+Time = list( np.arange(0, last_time+1, resolution) )
+
+# Length of the Time vector
+time_length = len(Time)
+
+list_of_N_veh = [None] * N
+
+
+
+for n in range(1, len(list_of_N_veh)):
+    ####### Assign names to Lead Vehicle Parameters ##########
+    
+    if n == 1: 
+        
+        # Lead vehicle position
+        xn1 = dfn1[xn1]
+        
+        # Lead vehicle speed
+        vn1 = dfn1[vn1]
+        
+    
+
+    ln1 = ln[n-1]
+
+    ####### Allocate Vectors ##################################
+
+    # free-flow speed
+    vn_ff = Vector{Union{Float64, Missing}}(missing, time_length)
+    
+    # car-following speed
+    vn_cf = Vector{Union{Float64, Missing}}(missing, time_length)
+    
+    # speed
+    vn = Vector{Union{Float64, Missing}}(missing, time_length)
+    
+
+    # position
+    xn = Vector{Union{Float64, Missing}}(missing, time_length)
+    
+
+    # spacing
+    sn = Vector{Union{Float64, Missing}}(missing, time_length)
+    
+
+    # speed difference
+    deltav = Vector{Union{Float64, Missing}}(missing, time_length)
+    
+
+    # acceleration rate
+    bn = Vector{Union{Float64, Missing}}(missing, time_length)
+    
+
+        ######## Initial values for Following vehicle ##################################
+
+    # speed
+    vn_ff[1] = vn_first[n]
+    vn_cf[1] = vn_first[n]
+    vn[1] = vn_first[n]
+
+    # position
+    xn[1] = xn_first[n]
+
+    # spacing
+    sn[1] = xn1[1] - xn_first[n]
+
+    # speed difference
+    deltav[1] = vn_first[n] - vn1[1]
+
+
+    ###### Gipps Calculations ############################
+    
+    for t in 2:(time_length-1)
+
+        ## free flow
+        vn_ff[t] = vn[t-1] + (2.5 * an * tau * (1 - (vn[t-1])/Vn)) * ((0.025 + (vn[t-1]/Vn))^(0.5))
+
+        ## car following
+        bcap_part_cf = (((vn1[t-1])^2)/bcap)
+
+        vn_cf[t] = (bn_const * tau) + sqrt(((bn_const^2) * (tau^2)) - (bn_const * (2 * (xn1[t-1] - ln1 - xn[t-1]) - (vn[t-1] * tau) - bcap_part_cf)))
+
+        ## gipps speed
+        if (ismissing.(vn1[t-1]))
+        
+        vn[t] = vn_ff[t]
+        
+        else 
+        
+        vn[t] = min(vn_ff[t], vn_cf[t] )
+        
+        
+
+
+        ### if the speed is negative, make it zero
+        vn[t] = ifelse(vn[t] < 0, 0, vn[t])
+        
+        
+        ## acceleration
+        bn[t-1] = (vn[t] - vn[t-1])/(resolution)
+        
+        
+        
+        ## position
+        xn[t] = xn[t-1] + (vn[t-1] * resolution) + (0.5 * bn[t-1] * (resolution)^2)
+        
+        
+        # spacing
+        sn[t] = xn1[t] - xn[t] - ln1
+        
+        # speed difference
+        deltav[t] = vn[t] - vn1[t]
+
